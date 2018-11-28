9056ad6708c2e26c4440147b0cbbb39d9c63a476
West Kraemer <kraemerwest@gmail.com>
2018-11-28 17:27:29 -0500

    add model object of Course and api url

--- JSONparsing/ViewController.swift
+++ JSONparsing/ViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
+//model object (put here rather than in a separate model folder for clarity and to be concise
+struct Course {
+    let id: Int?
+    let name: String?
+    let link: String?
+    let imageUrl: String?
+}
+
 class ViewController: UIViewController {
 
+  //api.letsbuildthatapp.com/jsondecodable/course
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
