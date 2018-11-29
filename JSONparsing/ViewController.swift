191acbba3becba6e9ee2ffbf17249f804c70d19f
West Kraemer <kraemerwest@gmail.com>
2018-11-28 19:08:24 -0500

    remove errors in code.

--- JSONparsing/ViewController.swift
+++ JSONparsing/ViewController.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 
+
+struct WebsiteDescription: Decodable {
+    var name : String
+    var description : String
+    var courses : [Course]
+    
+}
+
+
 struct Course: Decodable {
     let id: Int
     let name: String
@@ -22,14 +31,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
         guard let url = URL(string: jsonUrlString) else {return}
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             guard let data = data else {return}
             
             do {
-                let course = try JSONDecoder().decode(Course.self, from: data)
-                print(course.id)
+                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
+                print(websiteDescription.name, websiteDescription.description, websiteDescription.courses)
+                //let courses = try JSONDecoder().decode([Course].self, from: data)
             } catch let jsonErr {
                 print("Error serializing json", jsonErr)
             }
